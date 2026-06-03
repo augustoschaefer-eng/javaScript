fc08229413c1395a6de44d80a554352015a8a820
AUGUSTO CEZAR MORGENSTERN SCHAEFER <augusto_schaefer@estudante.sesisenai.org.br>
2026-06-03 19:19:27 -0300

    salvo

--- At Guiado/app.js
+++ At Guiado/app.js
@@ -1,10 +1,49 @@
 const btnTema = document.querySelector('#btn-tema');
 
-btnTema.addEventListener('click', () => {    
+btnTema.addEventListener('click', () => {   
 
-document.body.classList.toggle('dark'); 
+    document.body.classList.toggle('dark');
 
-const modoEscuro = document.body.classList.contains('dark');
+    const modoEscuro = document.body.classList.contains('dark');
 
-btnTema.textContent = modoEscuro ? '🌅 Modo Claro!' : '🌚 Modo escuro';
+    btnTema.textContent = modoEscuro ? '☀️ Modo Claro' : '🌙 Modo Escuro';
+});
+
+const btnToggleInfo = document.querySelector('#btn-toggle-info');
+const infoCorpo    = document.querySelector('#info-corpo');
+
+btnToggleInfo.addEventListener('click', () => {
+    const visivel = infoCorpo.style.display !== 'none';
+
+    if (visivel){
+        infoCorpo.style.display = 'none';
+        btnToggleInfo.textContent = 'Mostrar';
+    } else {
+        infoCorpo.style.display = 'block';
+        btnToggleInfo.textContent = 'Ocultar';
+    }
+
+    // visivel ? 
+    // (infoCorpo.style.display = 'none', btnToggleInfo.textContent = 'Mostrar') 
+    // : (infoCorpo.style.display = 'block', btnToggleInfo.textContent = 'Ocultar');
+});
+
+const btnAumentarJs  = document.querySelector('#btn-aumentar-js');
+const barraJs        = document.querySelector('#barra-js');
+const nivelJs       = document.querySelector('#nivel-js');
+
+btnAumentarJs.addEventListener('click', () => {
+    const nivelAtual = parseInt(nivelJs.textContent);
+
+    if (nivelAtual >= 100) {
+        btnAumentarJs.textContent = 'Javascript Dominado';
+        btnAumentarJs.disabled = 'true';
+        return;
+    }
+
+    const novoNivel = Math.min(nivelAtual + 10, 100);
+    
+    nivelJs.textContent = novoNivel + '%';
+
+    barraJs.style.width = novoNivel + '%';
 });
